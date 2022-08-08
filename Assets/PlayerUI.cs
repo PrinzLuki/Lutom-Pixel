30d6801bba92c71589b9d69658348d9739f38cc3
tomi <herceg133@gmail.com>
2022-08-08 17:42:00 +0200

    Added HealthUI

--- Assets/PlayerUI.cs
+++ Assets/PlayerUI.cs
@@ -7,25 +7,37 @@ using UnityEngine.UI;
 
 public class PlayerUI : NetworkBehaviour
 {
-    //public Image healthFill;
+    [Header("Health UI Large")]
+    public GameObject healthObjLarge;
+    public Slider healthFillLarge;
+    [Header("Health UI Small")]
+    public GameObject healthObjSmall;
+    public Slider healthFillSmall;
 
-    //public PlayerStats stats;
+    public PlayerStats stats;
 
-    //private void Start()
-    //{
-    //    stats = GetComponent<PlayerStats>();
-    //    stats.onHealthChanged.AddListener(CmdSetHealthFill);
-    //}
+    private void Start()
+    {
+        stats = GetComponent<PlayerStats>();
+        stats.onHealthChanged.AddListener(CmdSetHealthFill);
+        if (!hasAuthority)
+        {
+            healthObjSmall.SetActive(true);
+            healthObjLarge.SetActive(false);
+        }
 
-    //[Command]
-    //public void CmdSetHealthFill(float health, float maxHealth)
-    //{
-    //    RpcSetHealthFill(health, maxHealth);
-    //}
+    }
 
-    //[ClientRpc]
-    //public void RpcSetHealthFill(float health, float maxHealth)
-    //{
-    //    healthFill.fillAmount = health / maxHealth;
-    //}
+    [Command]
+    public void CmdSetHealthFill(float health, float maxHealth)
+    {
+        RpcSetHealthFill(health, maxHealth);
+    }
+
+    [ClientRpc]
+    public void RpcSetHealthFill(float health, float maxHealth)
+    {
+        healthFillLarge.value = health / maxHealth;
+        healthFillSmall.value = health / maxHealth;
+    }
 }
