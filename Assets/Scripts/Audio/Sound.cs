30f4c063ed51b33017743739aa95ab67cf6987f0
tomi <herceg133@gmail.com>
2022-08-17 20:18:30 +0200

    Added Jump Sound Effect

---
+++ Assets/Scripts/Audio/Sound.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class Sound
+{
+
+    public string name;
+
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+
+    [Range(-3f, 3f)]
+    public float pitch;
+
+    public bool loop = false;
+
+    public AudioType type;
+
+    [HideInInspector]
+    public AudioSource source;
+
+
+}
