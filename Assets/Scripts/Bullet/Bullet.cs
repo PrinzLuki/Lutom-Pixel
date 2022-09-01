1993b9c6c73d81a0b500b68a4483fabd9bfc9f46
tomi <herceg133@gmail.com>
2022-09-01 18:44:47 +0200

    Bullet Bug Fixed

--- Assets/Scripts/Bullet/Bullet.cs
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -8,19 +8,26 @@ public class Bullet : NetworkBehaviour
 {
     public BulletScriptableObject bulletScriptable;
     public Transform parent;
+    public bool bulletHit;
 
     public virtual void Start()
     {
+        bulletHit = false;
         Destroy(gameObject, bulletScriptable.timeUntilDestroyed);
     }
 
     public virtual void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.GetComponent<IDamageable>() == null) return;
+        if (other.gameObject.GetComponent<IDamageable>() != null && !bulletHit)
+        {
+            other.gameObject.GetComponent<IDamageable>().GetDamage(bulletScriptable.damage);
+            Destroy(gameObject);
+        }
+        else
+        {
+            bulletHit = true;
+        }
 
-        other.gameObject.GetComponent<IDamageable>().GetDamage(bulletScriptable.damage);
-
-        Destroy(gameObject);
     }
 
 }
